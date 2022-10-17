686bc17265610135fd714017b7201b8cbd7d6430
Helder Cambuta <63684025+heldercambuta@users.noreply.github.com>
2022-10-17 17:02:55 +0100

    Update the App

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
-  <div>
+  <slot>
     <HeaderComp/>
-  </div>
+    <PlacarComp/>
+    <MensagemComp/>
+    <NoticiasComp/>
+    <FooterComp/>
+  </slot>
 </template>
 
 <script>
 import HeaderComp from '@/components/HeaderComp.vue'
+import PlacarComp from '@/components/PlacarComp.vue'
+import MensagemComp from './components/MensagemComp.vue'
+import NoticiasComp from './components/NoticiasComp.vue'
+import FooterComp from './components/FooterComp.vue'
 
 export default {
   name: 'App',
   components: {
     HeaderComp,
-  }
+    PlacarComp,
+    MensagemComp,
+    NoticiasComp,
+    FooterComp
+}
 }
 </script>
 
